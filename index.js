119645288be930dd8cfff8cdbe65418fac3116d7
Stephen Kane <stephenw.kane@gmail.com>
2023-08-09 13:18:53 -0400

    determineCryptoCoinPurchases now passing tests

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { exchangeRates } from "./response.js";
+
 export function validatePercentagesAddUpTo100(purchasePercentages) {
   const percentagesTotal = Object.values(purchasePercentages).reduce(
     (total, percentage) => total + percentage,
@@ -7,6 +9,8 @@ export function validatePercentagesAddUpTo100(purchasePercentages) {
     throw new Error("specified percentages do not add up to 100");
 }
 
+// validate coinSyms exist in response, validate they have valid exchange rates
+
 export function determineCryptoCoinPurchases(
   dollars,
   purchasePercentages,
@@ -15,5 +19,14 @@ export function determineCryptoCoinPurchases(
   validatePercentagesAddUpTo100(purchasePercentages);
 
   const coinPurchaseAmounts = {};
+  for (const [coinSym, purchasePercentage] of Object.entries(
+    purchasePercentages
+  )) {
+    const coinPurchaseDollars = dollars * (purchasePercentage / 100);
+    const coinsPerDollarRate = parseFloat(exchangeRates.data.rates[coinSym]);
+    coinPurchaseAmounts[coinSym] = coinsPerDollarRate * coinPurchaseDollars;
+  }
   return coinPurchaseAmounts;
 }
+
+// console.log(determineCryptoCoinPurchases(100, {BTC: 70, ETH: 30}, exchangeRates))
